87855d1a92e50b3409b3ba861233916e2ba5d983
SanchoDev39 <121376575+SanchoDev39@users.noreply.github.com>
2024-09-22 23:54:30 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const image = document.querySelector(".avatar");
+image.addEventListener("click", () => {
+    image.src = "https://lh3.googleusercontent.com/pw/AP1GczOwdNyR6V33eU_a1Fm_z2ZggKEHHFhWexZumucqWVeUiAcmB6aDo75sMtToQAfLFcYVx5vIKQGm5KYrsCt-STNMRbPFIde-CtGr4I-EyomhJj88Rxjd0wWG04TMSkUa92O_Pa8ePLXA5bR88CfEGeTBzg=w946-h632-s-no-gm?authuser=0"
+})
+image.addEventListener("dblclick", () => {
+    image.src = "images/jorge.jpg"		
+    })
